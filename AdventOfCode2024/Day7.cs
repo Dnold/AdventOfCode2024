a864daa6083020b7f24ef97e4773db3e0e412ef5
Daniel Nold <75209804+Dnold@users.noreply.github.com>
2024-12-07 15:29:26 +0100

    Day 7 Part 2

--- AdventOfCode2024/Day7.cs
+++ AdventOfCode2024/Day7.cs
@@ -20,11 +20,24 @@ namespace AdventOfCode2024
         public Node left;
         public Node right;
     }
+    public class NodeTripple
+    {
+        public NodeTripple() { }
+        public NodeTripple(long value, string operation = "")
+        {
+            this.value = value;
+            this.operation = operation;
+        }
+        public long value;
+        public string operation;
+        public NodeTripple left;
+        public NodeTripple middle;
+        public NodeTripple right;
+    }
 
     public static class Day7
     {
-        public static List<Tuple<long, Node>> input;
-        public static List<int[]> calibrationValues = new List<int[]>();
+        public static List<Tuple<long, List<int>>> input;
         public static void Run()
         {
             string basePath = AppDomain.CurrentDomain.BaseDirectory;
@@ -46,43 +59,42 @@ namespace AdventOfCode2024
             long count = 0;
             for (int i = 0; i < input.Count; i++)
             {
-                for (int j = 0; j < calibrationValues[i].Length; j++)
+                for (int j = 0; j < input[i].Item2.Count; j++)
                 {
-                    Console.Write(calibrationValues[i][j] + " ");
+                    Console.Write(input[i].Item2[j] + " ");
                 }
+                Node constructedTree = ConstructTree(input[i].Item2);
                 Console.WriteLine("Expected Result: " + input[i].Item1);
-
-                if (ValidSolutionExists(input[i].Item2, input[i].Item1))
+                if (ValidSolutionExists(constructedTree, input[i].Item1))
                 {
-
                     Console.WriteLine("Valid Solution Exists");
                     count += input[i].Item1;
                 }
             }
-            Console.WriteLine("Number of Valid Solutions: " + count);
+            Console.WriteLine("Sum of Valid Solutions: " + count);
         }
-
         public static void Part2()
         {
-            
-        }
-      
-        public static bool ValidSolutionExists(Node node, long expectedResult)
-        {
-            if (node == null)
+            input = GetInput(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input", "Day7.txt"));
+            long count = 0;
+            Console.WriteLine("Part 2");
+            foreach (var item in input)
             {
-                return false;
+                // Hier können Sie eine andere Methode zur Baumkonstruktion aufrufen
+                NodeTripple root = ConstructTreeTripple(item.Item2);
+
+                Console.WriteLine("Expected Result: " + item.Item1);
+                if (ValidSolutionExistsTripple(root, item.Item1)){
+                    Console.WriteLine("Valid Solution Exists");
+                    count += item.Item1;
+                }
             }
-            if (node.left == null && node.right == null)
-            {
-                return node.value == expectedResult;
-            }
-            return ValidSolutionExists(node.left, expectedResult) || ValidSolutionExists(node.right, expectedResult);
+            Console.WriteLine("Sum of Valid Solutions: " + count);
         }
 
-        public static List<Tuple<long, Node>> GetInput(string path)
+        public static List<Tuple<long, List<int>>> GetInput(string path)
         {
-            List<Tuple<long, Node>> result = new List<Tuple<long, Node>>();
+            List<Tuple<long, List<int>>> result = new List<Tuple<long, List<int>>>();
 
             using (StreamReader streamReader = new StreamReader(path))
             {
@@ -101,34 +113,96 @@ namespace AdventOfCode2024
                         int val = int.Parse(value);
                         ints.Add(val);
                     }
-                    calibrationValues.Add(ints.ToArray());
-                    Node startNode = new Node(ints[0]);
-                    Queue<Node> nodesToProcess = new Queue<Node>();
-                    nodesToProcess.Enqueue(startNode);
-                    ints.RemoveAt(0);
-
-                    while (ints.Count > 0)
-                    {
-                        int currentValue = ints[0];
-                        ints.RemoveAt(0);
-                        int count = nodesToProcess.Count;
-
-                        for (int i = 0; i < count; i++)
-                        {
-                            Node currentNode = nodesToProcess.Dequeue();
-                            currentNode.left = new Node(currentNode.value * currentValue, "*" + currentValue);
-                            currentNode.right = new Node(currentNode.value + currentValue, "+" + currentValue);
-                            nodesToProcess.Enqueue(currentNode.left);
-                            nodesToProcess.Enqueue(currentNode.right);
-                        }
-                    }
-
-                    result.Add(new Tuple<long, Node>(expectedResult, startNode));
+                    result.Add(new Tuple<long, List<int>>(expectedResult, ints));
                 }
             }
 
             return result;
         }
+        public static bool ValidSolutionExists(Node node, long expectedResult)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.left == null && node.right == null)
+            {
+                return node.value == expectedResult;
+            }
+            return ValidSolutionExists(node.left, expectedResult) || ValidSolutionExists(node.right, expectedResult);
+        }
+        public static bool ValidSolutionExistsTripple(NodeTripple node, long expectedResult)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.left == null && node.right == null && node.middle == null)
+            {
+                return node.value == expectedResult;
+            }
+            return ValidSolutionExistsTripple(node.left, expectedResult) || ValidSolutionExistsTripple(node.right, expectedResult) || ValidSolutionExistsTripple(node.middle, expectedResult);
+        }
+        public static Node ConstructTree(List<int> ints)
+        {
+            if (ints == null || ints.Count == 0)
+            {
+                return null;
+            }
+
+            Node startNode = new Node(ints[0]);
+            Queue<Node> nodesToProcess = new Queue<Node>();
+            nodesToProcess.Enqueue(startNode);
+            ints.RemoveAt(0);
+
+            while (ints.Count > 0)
+            {
+                int currentValue = ints[0];
+                ints.RemoveAt(0);
+                int count = nodesToProcess.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    Node currentNode = nodesToProcess.Dequeue();
+                    currentNode.left = new Node(currentNode.value * currentValue, "*" + currentValue);
+                    currentNode.right = new Node(currentNode.value + currentValue, "+" + currentValue);
+                    nodesToProcess.Enqueue(currentNode.left);
+                    nodesToProcess.Enqueue(currentNode.right);
+                }
+            }
+
+            return startNode;
+        }
+        public static NodeTripple ConstructTreeTripple(List<int> ints)
+        {
+            if (ints == null || ints.Count == 0)
+            {
+                return null;
+            }
+            NodeTripple startNode = new NodeTripple(ints[0]);
+            Queue<NodeTripple> nodesToProcess = new Queue<NodeTripple>();
+            nodesToProcess.Enqueue(startNode);
+            ints.RemoveAt(0);
+            while (ints.Count > 0)
+            {
+                int currentValue = ints[0];
+                ints.RemoveAt(0);
+                int count = nodesToProcess.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    NodeTripple currentNode = nodesToProcess.Dequeue();
+                    currentNode.left = new NodeTripple(currentNode.value * currentValue, "*" + currentValue);
+                    currentNode.middle = new NodeTripple(currentNode.value + currentValue, "+" + currentValue);
+                    currentNode.right = new NodeTripple(long.Parse(currentNode.value.ToString() + currentValue.ToString()), "||" + currentValue);
+                    nodesToProcess.Enqueue(currentNode.left);
+                    nodesToProcess.Enqueue(currentNode.middle);
+                    nodesToProcess.Enqueue(currentNode.right);
+                }
+            }
+            return startNode;
+        }
+
         public static void PrintTree(Node node, string indent = "", bool isLeft = true)
         {
             if (node != null)
